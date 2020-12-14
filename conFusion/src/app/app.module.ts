bf0c43b28648131dcddbce777bc0dd71f2fb3cfd
Arjungowda <Arjunce15@gmail.com>
2020-12-14 15:12:55 +0530

    layouts nativescript

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "@nativescript/angular";
+import { HttpClientModule } from '@angular/common/http';
+
+import { DishService } from './services/dish.service';
+import { baseURL } from './shared/baseurl';
+import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
     bootstrap: [
@@ -12,14 +16,18 @@ import { ItemDetailComponent } from "./item/item-detail.component";
     ],
     imports: [
         NativeScriptModule,
-        AppRoutingModule
+        AppRoutingModule,
+        HttpClientModule
     ],
     declarations: [
         AppComponent,
-        ItemsComponent,
-        ItemDetailComponent
+        MenuComponent
     ],
-    providers: [],
+    providers: [
+        {provide:'BaseURL', useValue: baseURL},
+        DishService,
+        ProcessHTTPMsgService
+    ],
     schemas: [
         NO_ERRORS_SCHEMA
     ]
