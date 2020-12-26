9b9ab72eb060959a5e6e34a6b572aeb41e3daced
Arjungowda <Arjunce15@gmail.com>
2020-12-26 10:50:35 +0530

    login

--- conFusion/src/app/shared/drawer/drawer.component.ts
+++ conFusion/src/app/shared/drawer/drawer.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { TNSFontIconService } from 'nativescript-ngx-fonticon';
+import { login, LoginResult } from 'ui/dom';
+import { getString, setString } from 'tns-core-modules/application-settings';
+
 @Component({
     selector:'draw-content',
     templateUrl:'./shared/drawer/drawer.component.html'
@@ -9,4 +12,23 @@ export class DrawerComponent{
     constructor(private fontIcon:TNSFontIconService){
 
     }
+
+    displayLoginDialog(){
+        let options = {
+            title: "Login",
+            message: "Enter your login credentials",
+            userName: getString("userName", ""),
+            password: getString("password",""),
+            okButtonText: "Login",
+            cancelButtonText: "Cancel"
+        }
+
+        login(options)
+            .then((loginResult:LoginResult) =>{
+                setString("userName", loginResult.userName);
+                setString("password", loginResult.password);
+            },
+            () => {console.log("Login cancelled");
+        });
+        }
 }
