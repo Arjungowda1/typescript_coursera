4f6d7dc35e8ebdcb744a8da53809a00f755e8971
Arjungowda <Arjunce15@gmail.com>
2020-12-28 15:19:48 +0530

    Assignment 3

--- conFusion/src/app/reservation/reservation.component.ts
+++ conFusion/src/app/reservation/reservation.component.ts
@@ -4,6 +4,10 @@ import { Switch } from 'ui/dom/Component';
 import { Validators, FormBuilder, FormGroup} from '@angular/forms';
 import { ModalDialogService, ModalDialogOptions } from "@nativescript/angular/common";
 import { ReservationModalComponent } from "../reservationmodal/reservationmodal.component";
+import {  Page, View } from '@nativescript/core/ui';
+import { Animation, AnimationDefinition } from '@nativescript/core/ui/animation';
+import { CouchbaseService } from '../services/couchbase.service';
+import { DrawerPage } from '../shared/drawer/drawer.page';
 
 
 @Component({
@@ -11,14 +15,27 @@ import { ReservationModalComponent } from "../reservationmodal/reservationmodal.
     templateUrl: './reservation.component.html',
     styleUrls: ['./reservation.component.css']
 })
-export class ReservationComponent implements OnInit {
+export class ReservationComponent extends DrawerPage implements OnInit {
 
     reservation: FormGroup;
+    submittedNumOfGuests: number = 3;
+    submittedSmokingPref: boolean = false;
+    submittedDate: string = (new Date()).toDateString();
+    isSubmittedState: boolean = false;
+    reserveTableGridLayout: View;
+    reservationDetailsLayout: View;
 
-    constructor(private formBuilder: FormBuilder,
+    reservations: Array<any>;
+    docId: string = "reservations";
+
+    constructor(private changeDetectorRef: ChangeDetectorRef,
+        private formBuilder: FormBuilder,
         private modalService: ModalDialogService, 
-        private vcRef: ViewContainerRef) {
+        private vcRef: ViewContainerRef,
+        private page: Page,
+        private couchbaseService: CouchbaseService) {
 
+            super(changeDetectorRef);
             this.reservation = this.formBuilder.group({
                 guests: 3,
                 smoking: false,
@@ -27,7 +44,8 @@ export class ReservationComponent implements OnInit {
     }
 
     ngOnInit() {
-
+        this.reserveTableGridLayout = <View>this.page.getViewById<View>("reserveTableGridLayout");
+        this.reservationDetailsLayout = <View>this.page.getViewById<View>("reservationDetailsLayout");
     }
 
     onSmokingChecked(args) {
@@ -53,7 +71,32 @@ export class ReservationComponent implements OnInit {
     }
 
     onSubmit() {
-        console.log(JSON.stringify(this.reservation.value));
+        this.submittedNumOfGuests = this.reservation.value.guests;
+        this.submittedSmokingPref = this.reservation.value.smoking;
+        this.submittedDate = this.reservation.value.dateTime;
+
+
+
+        let doc = this.couchbaseService.getDocument(this.docId);
+
+        if (doc == null) {
+
+            console.log("This is the first reservation");
+            console.log(JSON.stringify(this.reservation.value));
+            this.reservations = [];
+            this.reservations.push(this.reservation.value);
+            this.couchbaseService.createDocument({ "reservations": this.reservations }, this.docId);
+
+        }
+        else {
+            this.reservations = doc.reservations;
+            this.reservations.push(this.reservation.value);
+            this.couchbaseService.updateDocument({ "reservations": this.reservations }, doc.id);
+        }
+
+        console.log(JSON.stringify(doc));
+        this.animateReservationFormOut();
+
     }
 
     createModalView(args) {
@@ -74,5 +117,68 @@ export class ReservationComponent implements OnInit {
                 }
             });
 
+    }
+
+
+    animateReservationFormOut() {
+        let definitions = new Array<AnimationDefinition>();
+        let a1: AnimationDefinition = {
+            target: this.reserveTableGridLayout,
+            scale: { x: 0, y: 0 },
+            opacity: 0,
+            duration: 500
+
+        };
+        definitions.push(a1);
+
+        /*
+        By default all the elements will have a scale of x:1 and y:1
+        and hence we have to first for the summary layout reduce the scale to (0,0)
+        and then in the next animmation we will increase the scale to (1,1)
+        */
+        let a2: AnimationDefinition = {
+            target: this.reservationDetailsLayout,
+            scale: { x: 0, y: 0 },
+            opacity: 0,
+            duration: 500
+
+        };
+        definitions.push(a2);
+
+
+
+        let animationSet = new Animation(definitions);
+
+        animationSet.play().then(() => {
+            this.isSubmittedState = true;
+            this.animateDetailsIn();
+        })
+            .catch((e) => {
+                console.log(e.message);
+            });
+    }
+
+    animateDetailsIn() {
+
+        let definitions = new Array<AnimationDefinition>();
+        let a1: AnimationDefinition = {
+            target: this.reservationDetailsLayout,
+            scale: { x: 1, y: 1 },
+            opacity: 1,
+            duration: 500
+
+        };
+        definitions.push(a1);
+
+
+
+        let animationSet = new Animation(definitions);
+
+        animationSet.play().then(() => {
+            
+        })
+            .catch((e) => {
+                console.log(e.message);
+            });
     }
 }
