84cf8d396a6e3f1013a8293d32b626b5505ca123
Arjungowda <Arjunce15@gmail.com>
2021-01-05 10:29:45 +0530

    email

--- conFusion/src/app/contact/contact.component.ts
+++ conFusion/src/app/contact/contact.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Inject, ChangeDetectorRef } from '@angular/core';
 import { DrawerPage } from '../shared/drawer/drawer.page';
+import { TNSFontIconService } from 'nativescript-ngx-fonticon';
+import * as Email from 'nativescript-email';
 
 @Component({
     selector: 'app-contact',
@@ -10,7 +12,8 @@ export class ContactComponent extends DrawerPage implements OnInit {
 
     constructor(
         private changeDetectorRef: ChangeDetectorRef,
-        @Inject('BaseURL') private BaseURL) {
+        @Inject('BaseURL') private BaseURL,
+        private fonticon: TNSFontIconService) {
         super(changeDetectorRef);
 
     }
@@ -18,5 +21,21 @@ export class ContactComponent extends DrawerPage implements OnInit {
     ngOnInit() {
 
 
+    }
+
+    sendEmail(){
+        Email.available()
+        .then((avail : boolean)=>{
+        if (avail){
+            Email.compose({
+                to: ['confusion@net.in'],
+                subject: 'Query',
+                body: 'Dear sir/madam'
+            });
+        }
+        else{
+            console.log("No email found")
+        }
+        })
     }
 }
